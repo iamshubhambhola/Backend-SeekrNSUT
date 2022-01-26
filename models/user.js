78c2caaff0d907995ab4b61e452f76be36e23093
Shubham Bhola <shubhambhola87@gmail.com>
2022-01-26 23:25:02 +0530

    created backend models and initialized user schema

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+
+
+//create schema
+const userSchema = new Schema({
+    email:{
+        type: String,
+        required: true, 
+        lowercase: true,
+        unique: true,
+        minlength: 3,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    data:{
+        type: Date,
+        default: Date.now
+    }, 
+},{
+    timestamps: true,
+},{collection: 'students'});
+ 
+
+const User = mongoose.model('User',userSchema); 
+
+module.exports = {User}; 
