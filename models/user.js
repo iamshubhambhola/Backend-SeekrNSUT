0fa9dd375203dcbfc95282d6aaeeb9cdab4728ec
Shubham Bhola <shubhambhola87@gmail.com>
2022-01-28 16:33:37 +0530

    Added fields in the Schemas

--- models/user.js
+++ models/user.js
@@ -8,10 +8,17 @@ var validateEmail = function(email) {
     return re.test(email)
 };
 
-
-
+ 
 //create schema
 const userSchema = new Schema({
+    name :{
+        type: String,
+        required: true,
+        required: 'Name is required'
+    },
+    profilePicture:{
+        type: String,
+    },    
     email:{
         type: String,
         required: true, 
@@ -22,6 +29,60 @@ const userSchema = new Schema({
         validate: [validateEmail, 'Please fill a valid email address'],
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
+    password : {
+        type: String, 
+        select: false,
+        required: true
+    },
+    role : {
+        type: String,
+        required: true, 
+    },
+    projects:[{type: Schema.Types.ObjectId, ref="Project" }],
+    researchPapers:[{type: Schema.Types.ObjectId, ref="Research" }],
+    socialLinks : [
+        {
+        githubLink: String,
+        googleScholarProfileLink: String,
+        linkedIn :String
+    }
+    ],
+    resumeId:{
+        type: String,
+        unique: true
+    },     
+    userBio: {
+        type: String,
+    },
+    skillSet:[{
+        type: Schema.Types.ObjectId,
+        ref: "Skills",
+    }],  
+    accomplishments: [{
+        title: String,
+        description: String,
+        date: {
+            type: Date,
+            default: Date.now
+        },
+    }], 
+    workExperience: [{
+        type: Schema.Types.ObjectId,
+        ref="WorkExperience"
+    }],
+    bookmarks: [{
+        type: Schema.Types.ObjectId,
+        ref: "Research"
+    },{
+        type: Schema.Types.ObjectId,
+        ref: "Project"
+    }],
+    followers:[{type: Schema.Types.ObjectId, ref="User" }],
+    following:[{type: Schema.Types.ObjectId, ref="User" }],
+    organisation: [{
+        type: Schema.Types.ObjectId,
+        ref="Organisation"
+    }],
     date:{
         type: Date,
         default: Date.now
