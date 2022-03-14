8567bd887745d0d55af5d9f0ff0e0c2d2320b80f
Shubham Bhola <iamshubhambhola>
2022-03-15 00:25:40 +0530

    created mongoDb cluster and connected db

--- models/user.js
+++ models/user.js
@@ -38,8 +38,8 @@ const userSchema = new Schema({
         type: String,
         required: true, 
     },
-    projects:[{type: Schema.Types.ObjectId, ref="Project" }],
-    researchPapers:[{type: Schema.Types.ObjectId, ref="Research" }],
+    projects:[{type: Schema.Types.ObjectId, ref:"Project" }],
+    researchPapers:[{type: Schema.Types.ObjectId, ref:"Research" }],
     socialLinks : [{
         githubLink: String,
         googleScholarProfileLink: String,
@@ -66,7 +66,7 @@ const userSchema = new Schema({
     }], 
     workExperience: [{
         type: Schema.Types.ObjectId,
-        ref="WorkExperience"
+        ref:"WorkExperience"
     }],
     bookmarksResearch: [{
         type: Schema.Types.ObjectId,
@@ -76,11 +76,11 @@ const userSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "Project"
     }],
-    followers:[{type: Schema.Types.ObjectId, ref="User" }],
-    following:[{type: Schema.Types.ObjectId, ref="User" }],
+    followers:[{type: Schema.Types.ObjectId, ref:"User" }],
+    following:[{type: Schema.Types.ObjectId, ref:"User" }],
     organisation: [{
         type: Schema.Types.ObjectId,
-        ref="Organisation"
+        ref:"Organisation"
     }],
     date:{
         type: Date,
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 5000;
-const db = require('./config/mongoose');
-const app =express();
+const db = require('./config/keys').mongoURI;
+const app = express();
+const bodyParser= require('body-parser');
+const mongoose = require('mongoose');
+//const bodyParser= require('body-parser');
+app.use(bodyParser.json());
 
-app.set('view engine' , 'ejs');
-app.set('views', path.join(__dirname , 'views'));
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(express.json({ extended: false }));
@@ -14,15 +17,25 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
- 
+
+
+
+const connectDB = async () => {
+    try { await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }); console.log('mongodb connected...'); } catch (err) {
+        console.log(err.message); //exit process 1 
+        process.exit(1);
+    }
+}
+connectDB();
+
 
 app.get('/', (req, res) => {
     res.send("hello")
-  })
+})
 
-app.listen(port , function(err){
-    if(err){
+app.listen(port, function (err) {
+    if (err) {
         console.log('error!');
     }
-    console.log(`yups! expreess server is running on port http://localhost:${port} : ` , port);
+    console.log(`yups! expreess server is running on port http://localhost:${port} : `, port);
 })
